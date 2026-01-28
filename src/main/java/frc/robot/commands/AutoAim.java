ebe25dba204b22c44eb262dd36f51c708b866741
Woodland4678 <cybercavs@woodland.on.ca>
2026-01-28 18:33:18 -0500

    Initial commit

---
+++ src/main/java/frc/robot/commands/AutoAim.java
@@ -0,0 +1,114 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.Optional;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class AutoAim extends Command {
+
+  CommandSwerveDrivetrain S_Swerve;
+
+  Optional<Alliance> ally = DriverStation.getAlliance();
+
+  // The x and y values of the hub.
+  Double hubX;
+  Double hubY;
+
+  double robotX;
+  double robotY;
+
+  // Angle to turn to for facing centre of the hub.
+  Double targetAngle;
+
+  PhoenixPIDController rController = new PhoenixPIDController(14.1, 0, 0.15);
+    private final SwerveRequest.FieldCentric m_driveRequestDrive = new SwerveRequest.FieldCentric()
+            .withDeadband(4 * 0.1).withRotationalDeadband(6 * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+
+  /** Creates a new AutoAim. */
+  public AutoAim(CommandSwerveDrivetrain S_Swerve) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.S_Swerve = S_Swerve;
+    addRequirements(S_Swerve);
+
+      rController.enableContinuousInput(-Math.PI, Math.PI);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (ally.isPresent()) {
+      if (ally.get() == Alliance.Red) {
+
+        hubX = 4.675 + 7.19;
+        hubY = 8.07/2;
+
+      } else if (ally.get() == Alliance.Blue) {
+
+        hubX = 4.675;
+        hubY = 8.07/2;
+      }
+    }
+    robotX = S_Swerve.getState().Pose.getX();
+    robotY = S_Swerve.getState().Pose.getY();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    robotX = S_Swerve.getState().Pose.getX();
+    robotY = S_Swerve.getState().Pose.getY();
+   
+    // Get the angle to turn to face the centre of the hub.
+    if (ally.isPresent()) {
+      if (ally.get() == Alliance.Red) {
+        // FInd the angle from the robot to the hub.
+        Double angleToTag = Math.atan((hubY - robotY) / ((robotX - hubX)));
+
+        // Find the angle the robot needs to turn to face the hub.
+        targetAngle = Math.PI*angleToTag/Math.abs(angleToTag) - angleToTag;
+      
+      } else {
+        // Get the angle to turn to facing the centre of the hub.
+        targetAngle = Math.atan((hubY - robotY)/(hubX - robotX));
+      }
+    }
+
+    // Turn the robot the correct angle.
+    double degrees = S_Swerve.getGyroValue();
+    double rSpeed = rController.calculate(Math.toRadians(degrees), targetAngle, Timer.getFPGATimestamp());
+    S_Swerve.setControl(m_driveRequestDrive.withVelocityX(0).withVelocityY(0).withRotationalRate(rSpeed));
+    SmartDashboard.putNumber("Auto Aim rspeed", rSpeed);
+    SmartDashboard.putNumber("Target Angle", targetAngle);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+    // Set all speeds to 0.
+    S_Swerve.setControl(m_driveRequestDrive.withVelocityX(0).withVelocityY(0).withRotationalRate(0));
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/commands/DriveOverBump.java
@@ -0,0 +1,107 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class DriveOverBump extends Command {
+  /** Creates a new DriveOverBump. */
+   PhoenixPIDController rController = new PhoenixPIDController(14.1, 0, 0.15);
+  CommandSwerveDrivetrain S_Swerve;
+  double angleToTarget;
+  double targetX = 5.223; //from blue alliance wall to middle of blue hub
+  double targetY = 4.035;
+  int state = 0;
+  int cnt = 0;
+  double xSpeed = 4.2;
+  double ySpeed = 0.0;
+  private final SwerveRequest.FieldCentric m_driveRequestDrive = new SwerveRequest.FieldCentric()
+            .withDeadband(4 * 0.1).withRotationalDeadband(6 * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+  public DriveOverBump(CommandSwerveDrivetrain S_Swerve) {
+    this.S_Swerve = S_Swerve;
+    addRequirements(S_Swerve);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    state = 0;
+    xSpeed = 4.2;
+    cnt = 0;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    S_Swerve.setControl(
+          m_driveRequestDrive.withVelocityX(xSpeed)
+              .withVelocityY(ySpeed)
+              .withRotationalRate(0)
+
+        );
+        //I conclude looking for one direction, then the other, then flat will be more consistent
+      switch(state) {
+        case 0:
+          if (Math.abs(S_Swerve.getGyroRoll())>3 || Math.abs(S_Swerve.getGyroPitch()) > 3) {
+            state++;
+          }
+        break;
+      /*  case 0:
+          if (S_Swerve.getGyroRoll() > 3) {
+            state++;
+          }
+        break;
+        case 1:
+          if (S_Swerve.getGyroRoll() < -3) {
+            state++;
+          }
+        break;*/
+        case 1:
+          if ((S_Swerve.getGyroRoll() > -1.5 && S_Swerve.getGyroRoll() < 1.5)&&(S_Swerve.getGyroPitch() > -1.5 && S_Swerve.getGyroPitch() < 1.5)) {
+            //state++;
+            cnt++;
+          }
+          else {
+            cnt = 0;
+          }
+          if (cnt > 5) {
+            state++;
+          }
+        break;
+        case 2:
+          xSpeed = 0;
+          ySpeed = 0;
+        break;
+      }
+      SmartDashboard.putNumber("Get Over Bump State", state);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    S_Swerve.setControl(
+          m_driveRequestDrive.withVelocityX(0)
+              .withVelocityY(0)
+              .withRotationalRate(0)
+
+        );
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
